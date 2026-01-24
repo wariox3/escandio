07bd0ea51b2f3e7fc434eecd49e2365004821d16
wariox3 <maestradaz3@gmail.com>
2026-01-24 09:09:38 -0500

    feat: remove redundant fields from GenEmpresa model and serializer

--- general/serializers/empresa.py
+++ general/serializers/empresa.py
@@ -6,7 +6,7 @@ class GenEmpresaSerializador(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = GenEmpresa
         fields = ['id', 'numero_identificacion','digito_verificacion','nombre_corto','direccion','ciudad','telefono','correo',
-                  'imagen','contenedor_id','rededoc_id','subdominio']
+                  'imagen','contenedor_id','subdominio']
         
     def to_representation(self, instance):
         ciudad_nombre = ""
@@ -15,23 +15,12 @@ class GenEmpresaSerializador(serializers.HyperlinkedModelSerializer):
             ciudad_nombre = instance.ciudad.nombre        
             if instance.ciudad.estado:
                 ciudad_estado_nombre = instance.ciudad.estado.nombre
-        tipo_persona_nombre = ""
-        if instance.tipo_persona:
-            tipo_persona_nombre = instance.tipo_persona.nombre
-        identificacion_nombre = ""
-        if instance.identificacion:
-            identificacion_nombre = instance.identificacion.nombre
-        regimen_nombre = ""
-        if instance.regimen:
-            regimen_nombre = instance.regimen.nombre
 
         region = config('DO_REGION')
         bucket = config('DO_BUCKET')
         return {
             'id': instance.id,            
             'numero_identificacion': instance.numero_identificacion,
-            'identificacion_id': instance.identificacion_id,
-            'identificacion_nombre': identificacion_nombre,
             'ciudad_id': instance.ciudad_id,
             'ciudad_nombre': ciudad_nombre,
             'ciudad_estado_nombre': ciudad_estado_nombre,
@@ -40,14 +29,7 @@ class GenEmpresaSerializador(serializers.HyperlinkedModelSerializer):
             'direccion': instance.direccion,
             'telefono': instance.telefono,
             'correo': instance.correo,
-            'regimen_id': instance.regimen_id,
-            'regimen_nombre': regimen_nombre,
-            'tipo_persona_id': instance.tipo_persona_id,
-            'tipo_persona_nombre': tipo_persona_nombre,
-            'rededoc_id' : instance.rededoc_id,
-            'imagen': f"https://{bucket}.{region}.digitaloceanspaces.com/{instance.imagen}",
-            'asistente_electronico': instance.asistente_electronico,
-            'asistente_predeterminado': instance.asistente_predeterminado
+            'imagen': f"https://{bucket}.{region}.digitaloceanspaces.com/{instance.imagen}"
         }   
 
 class GenEmpresaActualizarSerializador(serializers.HyperlinkedModelSerializer):
