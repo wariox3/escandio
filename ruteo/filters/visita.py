de68c31a12a748543136ece5f5b1e15b820a847b
Ottoniel Hernandez <ottoniek14@gmail.com>
2026-04-01 01:51:09 -0500

    feat: agregar filtro por placa de vehículo en visitas

--- ruteo/filters/visita.py
+++ ruteo/filters/visita.py
@@ -4,8 +4,9 @@ from ruteo.models.visita import RutVisita
 class NumberInFilter(django_filters.BaseInFilter, django_filters.NumberFilter):
     pass
 
-class VisitaFilter(django_filters.FilterSet):    
+class VisitaFilter(django_filters.FilterSet):
     franja_id__in = NumberInFilter(field_name='franja_id', lookup_expr='in')
+    despacho__vehiculo__placa = django_filters.CharFilter(field_name='despacho__vehiculo__placa', lookup_expr='icontains')
     class Meta:
         model = RutVisita
         fields = {'id': ['exact'],
@@ -17,8 +18,8 @@ class VisitaFilter(django_filters.FilterSet):
                   'estado_despacho': ['exact'],
                   'estado_decodificado': ['exact'],
                   'estado_decodificado_alerta': ['exact'],
-                  'fecha': ['gte', 'lte', 'gt', 'lt', 'exact'], 
-                  'fecha_entrega': ['gte', 'lte', 'gt', 'lt', 'exact'], 
+                  'fecha': ['gte', 'lte', 'gt', 'lt', 'exact'],
+                  'fecha_entrega': ['gte', 'lte', 'gt', 'lt', 'exact'],
                   'destinatario':['icontains'],
                   'franja_id': ['exact'],
                   'cita_inicio': ['isnull', 'gte', 'lte'],
