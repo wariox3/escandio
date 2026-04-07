cc36a0b2623d5bb9ca9777c7928f20f2bef208a5
Ottoniel Hernandez <ottoniek14@gmail.com>
2026-04-07 11:57:39 -0500

    fix: importar RutVehiculo en visita views para limpieza de despachos huerfanos

--- ruteo/views/visita.py
+++ ruteo/views/visita.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from ruteo.models.visita import RutVisita
 from ruteo.models.despacho import RutDespacho
+from ruteo.models.vehiculo import RutVehiculo
 from ruteo.models.franja import RutFranja
 from ruteo.models.flota import RutFlota
 from general.models.configuracion import GenConfiguracion
